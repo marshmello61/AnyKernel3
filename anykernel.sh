e96e6e839befe7e53c10b8d091143d76a4b0c400
Mayur Varde <ultramayur123@gmail.com>
2022-02-23 18:35:26 +0530

    This whole time I was shipping module without actually shipping them :/

--- anykernel.sh
+++ anykernel.sh
@@ -6,7 +6,7 @@
 properties() { '
 kernel.string=Mello Oof Ultra Pro Max Super Duper Plus Kernel by Mayur @marshmello_61
 do.devicecheck=1
-do.modules=0
+do.modules=1
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
